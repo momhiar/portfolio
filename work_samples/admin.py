8edba9eb44098d8fb3b16dd4e8f3ce9f160ab7d1
Mohammad Esmaeili <esmaeili@dev.bigdata.rightel.ir>
2023-09-17 11:04:38 +0330

    register worksample models on admin

--- work_samples/admin.py
+++ work_samples/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(WorkSampleImage)
+admin.site.register(WorkSample)
+admin.site.register(WorkSampleTag)
