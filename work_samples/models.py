5dd871e91557920464aa554c4d71857dfe57e977
Mohammad Esmaeili <esmaeili@dev.bigdata.rightel.ir>
2023-09-17 11:03:44 +0330

    add: work samples image and tags

--- work_samples/models.py
+++ work_samples/models.py
@@ -1,8 +1,23 @@
 from django.db import models
-
+from tinymce import models as tinymce_models
 # Create your models here.
+
+
+class WorkSampleImage(models.Model):
+    title = models.CharField(max_length=75)
+    description = models.CharField(max_length=150)
+    alt = models.CharField(max_length=200)
+    picture = models.ImageField(upload_to='work-samples')
+
+
+class WorkSampleTag(models.Model):
+    title = models.CharField(max_length=25)
+    description = models.CharField(max_length=150)
 
 
 class WorkSample(models.Model):
     title = models.CharField(max_length=120)
     description = models.CharField(max_length=150)
+    text = tinymce_models.HTMLField()
+    gallery = models.ManyToManyField(WorkSampleImage)
+    tags = models.ManyToManyField(WorkSampleTag)
