ffeac9497cf684254c632d6b60e4c63449bbdcb8
Mohammad Esmaeili <esmaeili@dev.bigdata.rightel.ir>
2023-09-18 12:23:03 +0330

    add: model admins

--- work_samples/admin.py
+++ work_samples/admin.py
@@ -1,6 +1,35 @@
 from django.contrib import admin
 from .models import *
+from django.utils.html import mark_safe
 # Register your models here.
-admin.site.register(WorkSampleImage)
-admin.site.register(WorkSample)
-admin.site.register(WorkSampleTag)
+
+
+class WorkSampleImageAdmin(admin.ModelAdmin):
+    list_display = [
+        'title',
+        'display_picture_preview'
+    ]
+
+    def display_picture_preview(self, obj):
+        return mark_safe(
+            f'<img style="max-width: 150px; max-height:150px" src="/media/{obj.picture}" />'
+        )
+
+
+class WorkSampleTagAdmin(admin.ModelAdmin):
+    list_display = [
+        'title'
+    ]
+
+
+class WorksampleAdmin(admin.ModelAdmin):
+    list_display = [
+        'title',
+    ]
+    list_filter = ['tags']
+    search_fields = ['title']
+
+
+admin.site.register(WorkSampleImage, WorkSampleImageAdmin)
+admin.site.register(WorkSample, WorksampleAdmin)
+admin.site.register(WorkSampleTag, WorkSampleTagAdmin)
